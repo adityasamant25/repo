470521e1598afabb7444f8dc23f4e7543b066e92
adityasamant <adityasamant@gmail.com>
2017-05-10 19:04:04 +0530

    Added MySQL support to Spring Boot

---
+++ course-api-mysql/src/main/java/io/javabrains/CourseApiMysqlApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiMysqlApplication.class, args);
+	}
+}
