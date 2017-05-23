730ee0b3b921dc9ceb5f55bcecf88410decc8812
adityasamant <adityasamant@gmail.com>
2017-05-23 14:06:41 +0530

    Added a project to demonstrate Spring Security

---
+++ simple-security/src/main/java/com/spring/security/controller/MultipleAuthController.java
@@ -0,0 +1,13 @@
+package com.spring.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MultipleAuthController {
+	
+    @RequestMapping("/api/ping")
+    public String getPing() {
+        return "OK";
+    }
+}
