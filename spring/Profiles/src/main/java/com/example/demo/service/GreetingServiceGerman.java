4fff8205244f65c3dfb1aca4aa99a75e96bb7962
ADSAMANT <adityasamant@gmail.com>
2019-03-27 13:27:53 +0530

    Spring Boot Training

---
+++ spring/Profiles/src/main/java/com/example/demo/service/GreetingServiceGerman.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("german")
+public class GreetingServiceGerman implements GreetingService {
+
+	@Override
+	public String sayHello(String name) {
+		// TODO Auto-generated method stub
+		return "Halo " + name;
+	}
+
+	
+}
