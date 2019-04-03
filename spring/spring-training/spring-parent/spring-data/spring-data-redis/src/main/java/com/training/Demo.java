928f5ed6ba0670601dea55f969333f910f329666
ADSAMANT <adityasamant@gmail.com>
2019-04-03 12:49:30 +0530

    worked on Redis operations and java types

--- spring/spring-training/spring-parent/spring-data/spring-data-redis/src/main/java/com/training/Demo.java
+++ spring/spring-training/spring-parent/spring-data/spring-data-redis/src/main/java/com/training/Demo.java
@@ -1,11 +1,14 @@
 package com.training;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.support.collections.DefaultRedisMap;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,15 +28,30 @@ public class Demo implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		reset();
 		caching();
+		operations();
+		javaTypes();
 	}
 
 	private void reset() {
-		this.template.delete(Arrays.asList("boot"));
+		this.template.delete(Arrays.asList("boot", "abc", "data"));
 	}
 
 	private void caching() {
 		logger.info("----> 1 " + this.service.execute("boot"));
 		logger.info("----> 2 " + this.service.execute("boot"));
 		logger.info("----> 3 " + this.service.execute("boot"));
+	}
+	
+	private void operations() {
+		ValueOperations<String, String> ops = this.template.opsForValue();
+		Arrays.asList(1, 2, 3, 6).forEach((i) -> ops.increment("abc", i));
+		logger.info("Operations result: " + ops.get("abc"));
+	}
+	
+	private void javaTypes() {
+		Map<String, String> map = new DefaultRedisMap<>("data", this.template);
+		map.put("spring", "boot");
+		map = new DefaultRedisMap<>("data", this.template);
+		logger.info("Map value after Redis recreation: " + map.get("spring"));
 	}
 }
