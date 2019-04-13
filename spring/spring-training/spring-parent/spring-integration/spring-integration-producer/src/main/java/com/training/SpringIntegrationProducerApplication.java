6ec0d7a8817e3054861a0a8445d4ac1a244142de
ADSAMANT <adityasamant@gmail.com>
2019-04-13 17:45:05 +0530

    spring cloud stream with RabbitMQ

---
+++ spring/spring-training/spring-parent/spring-integration/spring-integration-producer/src/main/java/com/training/SpringIntegrationProducerApplication.java
@@ -0,0 +1,44 @@
+package com.training;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableBinding(MessageChannels.class)
+@RestController
+@SpringBootApplication
+public class SpringIntegrationProducerApplication {
+
+	private final MessageChannels channels;
+
+	@Autowired
+	public SpringIntegrationProducerApplication(MessageChannels channels) {
+		this.channels = channels;
+	}
+	
+	@GetMapping(value = "/greet/{name}")
+	void greet(@PathVariable String name) {
+		Message<String> msg = MessageBuilder.withPayload(name).build();
+		this.channels.output().send(msg);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationProducerApplication.class, args);
+	}
+
+}
+
+interface MessageChannels {
+
+	@Output
+	MessageChannel output();
+
+}
