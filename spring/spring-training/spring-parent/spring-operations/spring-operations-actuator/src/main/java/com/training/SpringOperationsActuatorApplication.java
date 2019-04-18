c5df1180f2a108b1e603abc02234a95ad98c93e7
ADSAMANT <adityasamant@gmail.com>
2019-04-18 19:27:34 +0530

    spring boot actuator

---
+++ spring/spring-training/spring-parent/spring-operations/spring-operations-actuator/src/main/java/com/training/SpringOperationsActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOperationsActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOperationsActuatorApplication.class, args);
+	}
+
+}
