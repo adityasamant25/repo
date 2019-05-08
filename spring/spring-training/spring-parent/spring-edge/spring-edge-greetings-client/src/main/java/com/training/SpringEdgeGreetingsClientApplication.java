0d126aaabdbfe5597a37d8a0e20c0dc376507ed7
ADSAMANT <adityasamant@gmail.com>
2019-05-08 17:12:17 +0530

    Zuul Filters - RateLimiting using Google Guava

--- spring/spring-training/spring-parent/spring-edge/spring-edge-greetings-client/src/main/java/com/training/SpringEdgeGreetingsClientApplication.java
+++ spring/spring-training/spring-parent/spring-edge/spring-edge-greetings-client/src/main/java/com/training/SpringEdgeGreetingsClientApplication.java
@@ -1,9 +1,22 @@
 package com.training;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,6 +25,47 @@ public class SpringEdgeGreetingsClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEdgeGreetingsClientApplication.class, args);
+	}
+
+}
+
+@Component
+class RateLimitingZuulFilter extends ZuulFilter {
+
+	private final RateLimiter rateLimiter = RateLimiter.create(1.0 / 30.0);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		try {
+			RequestContext currentContext = RequestContext.getCurrentContext();
+			HttpServletResponse response = currentContext.getResponse();
+
+			if (!this.rateLimiter.tryAcquire()) {
+				response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+				response.getWriter().append(HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+				currentContext.setSendZuulResponse(false);
+			}
+		} catch (IOException e) {
+			ReflectionUtils.rethrowRuntimeException(e);
+		}
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return Ordered.HIGHEST_PRECEDENCE + 100;
 	}
 
 }
