b8a2215344eb5fbfe0dc5df119079723f468fa97
ADSAMANT <adityasamant@gmail.com>
2019-05-08 18:16:05 +0530

    API Gateway with a RestTemplate

--- spring/spring-training/spring-parent/spring-edge/spring-edge-greetings-client/src/main/java/com/training/SpringEdgeGreetingsClientApplication.java
+++ spring/spring-training/spring-parent/spring-edge/spring-edge-greetings-client/src/main/java/com/training/SpringEdgeGreetingsClientApplication.java
@@ -4,14 +4,23 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.common.util.concurrent.RateLimiter;
 import com.netflix.zuul.ZuulFilter;
@@ -23,13 +32,46 @@ import com.netflix.zuul.exception.ZuulException;
 @SpringBootApplication
 public class SpringEdgeGreetingsClientApplication {
 
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEdgeGreetingsClientApplication.class, args);
 	}
 
 }
 
-@Component
+@RestController
+class GreetingsApiGatewayRestController {
+
+	private final RestTemplate restTemplate;
+
+	@Autowired
+	public GreetingsApiGatewayRestController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hi/{name}")
+	String greet(@PathVariable String name) {
+		ResponseEntity<Greeting> responseEntity = this.restTemplate.exchange("http://greetings-service/greetings/{name}", HttpMethod.GET, null,
+				Greeting.class, name);
+		return responseEntity.getBody().getGreeting();
+	}
+}
+
+class Greeting {
+	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+}
+
+//@Component
 class RateLimitingZuulFilter extends ZuulFilter {
 
 	private final RateLimiter rateLimiter = RateLimiter.create(1.0 / 30.0);
@@ -41,7 +83,7 @@ class RateLimitingZuulFilter extends ZuulFilter {
 
 	@Override
 	public Object run() throws ZuulException {
-		
+
 		try {
 			RequestContext currentContext = RequestContext.getCurrentContext();
 			HttpServletResponse response = currentContext.getResponse();
